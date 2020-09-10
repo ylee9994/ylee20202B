57a418a813c24015a7992f59770cd0086d3e7758
ylee <gabrieloboe@gmail.com>
2020-09-10 14:36:15 +0900

    2020 0903

--- 02thread/src/main/java/com/ylee/a02thread/MainActivity.java
+++ 02thread/src/main/java/com/ylee/a02thread/MainActivity.java
@@ -3,6 +3,7 @@ package com.ylee.a02thread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -30,11 +31,50 @@ public class MainActivity extends AppCompatActivity {
         tvSeek = findViewById(R.id.tvSeek);
         btnStart = findViewById(R.id.btnStart);
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                for(int i=0; i<100; ++i){
+//                    sb1.setProgress(sb1.getProgress()+2);
+//                    sb2.setProgress(sb2.getProgress()+1);
+//                    SystemClock.sleep(100);
+//                }
+                new Thread(){
+                    public void run(){
+                        for(int i=0; i< 100; i=i+2){
+                            sb1.setProgress(sb1.getProgress()+2);
+                            SystemClock.sleep(100);
+                        }
+                    }
+                }.start();
+
+                new Thread(){
+                    public void run(){
+                        for(int i=0; i< 100; i=i+1){
+                            sb2.setProgress(sb2.getProgress()+1);
+                            SystemClock.sleep(100);
+                        }
+                    }
+                }.start();
+            }
+        });
         sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                tvSeek.setText("진행율: " + progress + "%" );
+                tvSeek.setText("진행율1: " + progress + "%" );
 
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {  }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {  }
+        });
+
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvSeek.setText("진행율2: " + progress + "%" );
+                pb1.setProgress(progress);
             }
 
             @Override
