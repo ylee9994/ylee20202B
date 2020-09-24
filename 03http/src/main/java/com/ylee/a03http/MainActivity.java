26a7ff88a3fb930c663c81f9d9d5ae1e7784c962
ylee <gabrieloboe@gmail.com>
2020-09-24 15:20:42 +0900

    2020 0903

--- 03http/src/main/java/com/ylee/a03http/MainActivity.java
+++ 03http/src/main/java/com/ylee/a03http/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ylee.a03http;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     String naverurl = "https://m.naver.com";
+    String urlStr = "https://api.openweathermap.org/data/2.5/forecast?&APPID=3e0c533ec49a146af626992e6bcce23f&lat=36.1444292&lon=128.391079&mode=xml&units=metric&cnt=15";;
+    String openweatherapi = "https://api.openweathermap.org/data/2.5/weather?&mode=json&units=metric&appid=3e0c533ec49a146af626992e6bcce23f&q=seoul";
+    String busapi = "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey=vJm8%2BGpTs1b6mG2%2FB%2Ff31iZqhHXyrrnEN8FjnIBUUwaD6fwqu1kY0dDpH4LBNbNVONA43%2Bt88%2BUWxFloMwKv6A%3D%3D&busRouteId=100100414";
+    String airpollutionapi="http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=vJm8%2BGpTs1b6mG2%2FB%2Ff31iZqhHXyrrnEN8FjnIBUUwaD6fwqu1kY0dDpH4LBNbNVONA43%2Bt88%2BUWxFloMwKv6A%3D%3D&numOfRows=20&pageNo=1&sidoName=서울&ver=1.3";
+
     String htmldata = null;
     String sourceurl = null;
 
@@ -35,18 +44,50 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RequestThread rthread = new RequestThread();
-                rthread.start();
-               // SystemClock.sleep(5000);
+//                RequestThread rthread = new RequestThread();
+////                rthread.start();
+                RequestTask requestTask = new RequestTask();
+                requestTask.execute();
+                //SystemClock.sleep(5000);
                 // textView.setText(htmldata);
             }
         });
     }
 
+    class RequestTask extends AsyncTask<Integer, Integer, Integer>{
+        @Override
+        protected void onPreExecute() {
+           textView.setText("AsyncTask 돌고있음");
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            textView.setText(htmldata);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            htmldata = getHTTPdata(sourceurl);
+            return null;
+        }
+    }
+
+
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            textView.setText((String) msg.obj);
+        }
+    };
+
     class RequestThread extends Thread{
         public void run(){
             htmldata = getHTTPdata(sourceurl);
-            textView.setText(htmldata);
+            //textView.setText(htmldata);
+            Message message = handler.obtainMessage();
+            message.obj = htmldata;
+            handler.sendMessage(message);
          }
     }
 
